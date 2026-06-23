f9025e0abf6308339efd11bcb6dbdc2d0dfe6dcb
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-06-22 23:10:01 -0300

    Update script.js

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        themeSystem: 'standard',
+        // Aqui o calendário busca os dados automaticamente
+        events: '../data/eventos.json' 
+    });
+    calendar.render();
+});
 
